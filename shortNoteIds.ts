bcf3f1f127b8747cffb5aa74629d843ca0bf7e61
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2025-10-26 07:37:31 -0700

    add a detailed comment + ensure n is an integer

--- shortNoteIds.ts
+++ shortNoteIds.ts
@@ -1,14 +1,24 @@
+// toBaseN converts the non-negative integer `n` to a base-`${digits.length}`
+// representation in string form, using the characters in the string `digits` as
+// the digits. For example, toBaseN(10, 'AB') === 'BABA'.
+//
+// Caveats:
+// - Negative numbers aren't supported.
+// - Non-integers aren't supported.
+// - `digits` must be at least 2 characters long.
+// - We assume that each character in `digits` is unique.
 export function toBaseN(n: number, digits: string): string {
 	if (n < 0) {
 		throw `negative numbers are not supported`;
+	}
+	if (!Number.isInteger(n)) {
+		throw `non-integers are not supported`;
 	}
 
 	const base = digits.length;
 	if (base < 2) {
 		throw `toBaseN() got invalid digit string: ${JSON.stringify(digits)}`;
 	}
-
-	// TODO: validate `ds` is all unique
 
 	if (n == 0) return digits[0];
 
