1487f5152f60bbaab6c6e539bf243ff0d4c8d06d
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2025-10-26 07:22:23 -0700

    delete most default code, rename classes, stub settings

--- main.ts
+++ main.ts
@@ -1,89 +1,57 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface ShortNoteIdsPluginSettings {
+	epochYear: number;
+	newFileLocation: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: ShortNoteIdsPluginSettings = {
+	epochYear: -1,
+	newFileLocation: '',
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ShortNoteIdsPlugin extends Plugin {
+	settings: ShortNoteIdsPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// Configure the settings tab
+		this.addSettingTab(new ShortNoteIdsSettingTab(this.app, this));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
 
 		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// this.addCommand({
+		// 	id: 'open-sample-modal-simple',
+		// 	name: 'Open sample modal (simple)',
+		// 	callback: () => {
+		// 		new SampleModal(this.app).open();
+		// 	}
+		// });
 	}
 
 	onunload() {
 
 	}
 
+	generateInitialSettings(): ShortNoteIdsPluginSettings {
+		return {
+			...DEFAULT_SETTINGS,
+			epochYear: new Date().getFullYear(),
+		};
+	}
+
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		// Set epoch year on initial load
+		const savedSettings = await this.loadData();
+		console.log('short note IDs - loaded settings:', savedSettings);
+		const firstLoad = savedSettings == null;
+
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, savedSettings);
+		if (firstLoad) {
+			console.log('first load - persisting essential settings')
+			this.saveSettings();
+		}
 	}
 
 	async saveSettings() {
@@ -91,26 +59,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ShortNoteIdsSettingTab extends PluginSettingTab {
+	plugin: ShortNoteIdsPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ShortNoteIdsPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,15 +72,22 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
+		// New file location
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('New file location')
+			.setDesc('The folder path where new unique notes should be created.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Example: folder 1/folder')
+				.setValue(this.plugin.settings.newFileLocation)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.newFileLocation = value;
 					await this.plugin.saveSettings();
 				}));
+
+		// TODO: New file template
+
+		new Setting(containerEl)
+			.setName('Epoch year')
+			.setDesc('The year ')
 	}
 }
